96deeb5c5830a9bc417f214f0188a2640ad15c54
wengwenwen <wjy151515@163.com>
2015-04-10 16:01:03 +0800

    新增的数据库处理实体类

---
+++ src/com/example/readjson/db/WeatherDB.java
@@ -0,0 +1,87 @@
+package com.example.readjson.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.readjson.cityDomain.city;
+import com.example.readjson.cityDomain.province;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class WeatherDB {
+	public static final String DB_NAME = "weather_db";
+	public static final int VERSION =1;
+	private static WeatherDB weatherDB;
+	private SQLiteDatabase db;
+	private WeatherDB(Context context){
+		WeatherOpenHelper dbHelper = new WeatherOpenHelper(context, DB_NAME, null, VERSION);
+		db= dbHelper.getWritableDatabase();
+	}
+	public synchronized static WeatherDB getInstance(Context context ){
+		if(weatherDB==null){
+			weatherDB = new WeatherDB(context);
+		}
+		return weatherDB;
+		
+	}
+	public void SaveProvince(province p){
+		if(p !=null){
+			ContentValues values = new ContentValues();
+			values.put("province_name", p.getProvince());
+			db.insert("Province", null, values);
+			
+		}
+		
+	}
+	public List<province> loadProvince(){
+		List<province> list = new ArrayList<province>();
+		Cursor cursor = db.query("Province", null, null, null, null, null, null);
+		if(cursor.moveToFirst()){
+			do{
+				province Province = new province();
+				Province.setProvinceId(cursor.getInt(cursor.getColumnIndex("id")));
+				Province.setProvince(cursor.getString(cursor.getColumnIndex("province_name")));
+				list.add(Province);
+				
+				
+			}while(cursor.moveToNext());
+		}
+		return list;
+	}
+	public void saveCity(city City){
+		if(City !=null){
+			ContentValues values = new ContentValues();
+			values.put("city_name",City.getCityName());
+			values.put("city_code",City.getCityId());
+			values.put("province_id",City.getProvinceId());
+			
+			db.insert("City", null, values);
+			
+		}
+	}
+	public List<city> loadCity(int provinceId){
+		List<city> list = new ArrayList<city>();
+		Cursor cursor =db.query("City", null, "province_id = ?", new String[]{String.valueOf(provinceId)}, 
+				null, null, null);
+		if(cursor.moveToFirst()){
+			do {
+				city City = new city();
+				City.setCityId(cursor.getColumnName(cursor.getColumnIndex("city_code")));
+				City.setCityName(cursor.getColumnName(cursor.getColumnIndex("city_name")));
+				City.setProvinceId(provinceId);
+				list.add(City);
+				
+			} while (cursor.moveToNext());
+		}
+		return null;
+		
+	}
+	
+	
+	
+	
+
+}
